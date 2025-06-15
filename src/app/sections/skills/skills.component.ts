96ffd5dae6afa023d54becec940f667236fd0853
kailash gayari <kailash.g@amal.com>
2025-06-15 16:06:44 +0530

    Implement skills section with dynamic data display; add responsive styles and animations for improved user experience.

--- src/app/sections/skills/skills.component.ts
+++ src/app/sections/skills/skills.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { ProfileService } from '../../core/services/profile.service';
+import { KeyValuePipe, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [NgFor,KeyValuePipe,NgIf],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
+  skills: Record<string, string[]> | null = null;
 
+  constructor(private profileService: ProfileService) {}
+
+  ngOnInit(): void {
+     this.profileService.getProfile().subscribe(data=>{
+      console.log('profile', data)
+      this.skills = data.skills
+     });
+
+
+  }
 }
