7bcefa0844dea82a8098e7e6a77ac77190a4f869
kailash gayari <kailash.g@amal.com>
2025-06-15 16:39:10 +0530

    Add projects section with dynamic data display; implement HTML structure and styles for improved presentation.

--- src/app/sections/projects/projects.component.ts
+++ src/app/sections/projects/projects.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { ProfileService } from '../../core/services/profile.service';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [NgIf,NgFor],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  projects: any
+  constructor(private profileService: ProfileService) {}
 
+    ngOnInit(): void {
+      this.profileService.getProfile().subscribe((data) => {
+        this.projects = data?.projects || [];
+      });
+    }
 }
