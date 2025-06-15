f429c98b2990917d05066d7b6a23eb5dba3cf38f
kailash gayari <kailash.g@amal.com>
2025-06-15 15:16:41 +0530

    Implement hero section with dynamic profile data; add styles for layout and animations.

--- src/app/sections/hero/hero.component.ts
+++ src/app/sections/hero/hero.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { ProfileService } from '../../core/services/profile.service';
+import { KeyValuePipe, NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [NgStyle,NgIf,NgClass,KeyValuePipe,NgFor],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
 export class HeroComponent {
+  profile: any;
 
+  constructor(private profileService: ProfileService) {}
+
+  ngOnInit(): void {
+    this.profileService.getProfile().subscribe(data => {
+      this.profile = data;
+    });
+  }
 }
