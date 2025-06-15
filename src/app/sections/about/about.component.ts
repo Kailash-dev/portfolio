1aba1ecb6cf17c14b279cf89929926d5fbb8a61e
kailash gayari <kailash.g@amal.com>
2025-06-15 16:06:29 +0530

    Enhance About section with dynamic profile data display; implement responsive styles and animations for improved user experience.

--- src/app/sections/about/about.component.ts
+++ src/app/sections/about/about.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-
+import { ProfileService } from '../../core/services/profile.service';
+import { KeyValuePipe, NgClass, NgFor, NgIf, NgStyle, NgSwitch } from '@angular/common';
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [NgStyle,NgIf,NgClass,KeyValuePipe,NgFor,NgSwitch],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  about: any;
 
+  constructor(private profileService: ProfileService) {}
+
+  ngOnInit(): void {
+    this.profileService.getProfile().subscribe((data) => {
+      this.about = data.about;
+    });
+  }
 }
