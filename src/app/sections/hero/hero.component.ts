606e40a78e7cfb98556ef3a5147f75ec7a6cd20f
kailash gayari <kailash.g@amal.com>
2025-06-24 21:56:23 +0530

    Refactor app layout: replace sidebar with header and footer, enhance hero component styling

--- src/app/sections/hero/hero.component.ts
+++ src/app/sections/hero/hero.component.ts
@@ -1,22 +1,18 @@
 import { Component } from '@angular/core';
 import { ProfileService } from '../../core/services/profile.service';
 import { KeyValuePipe, NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
+import {MatButton, MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [NgStyle,NgIf,NgClass,KeyValuePipe,NgFor],
+  imports: [NgStyle,NgIf,NgClass,KeyValuePipe,NgFor, MatButtonModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
 export class HeroComponent {
-  profile: any;
-
-  constructor(private profileService: ProfileService) {}
-
-  ngOnInit(): void {
-    this.profileService.getProfile().subscribe(data => {
-      this.profile = data;
-    });
+  scrollToContact() {
+    const el = document.getElementById('contact');
+    el?.scrollIntoView({ behavior: 'smooth' });
   }
 }
